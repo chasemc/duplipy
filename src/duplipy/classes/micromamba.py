21ec8875cca9587bbf26b61204a6a89a7185d6cc
chasemc <18691127+chasemc@users.noreply.github.com>
2022-05-30 11:46:23 -0500

    temp

--- src/duplipy/classes/micromamba.py
+++ src/duplipy/classes/micromamba.py
@@ -2,29 +2,70 @@ import tarfile
 from pathlib import Path
 import subprocess
 from duplipy.utils.logging_handler import log
+import os
 
-MICROMAMBA_HOOK='eval "$(./bin/micromamba shell hook -s posix)"'
+MICROMAMBA_HOOK = 'eval "$(./bin/micromamba shell hook -s posix)"'
 
 
-class MM():
-    def run_micromamba(self, mm_path='/Users/chase/Downloads/temp/micromamba', cl_args=None):
+class MM:
+    def run_micromamba(
+        self, mm_path="/Users/chase/Downloads/temp/micromamba", cl_args=None
+    ):
         if mm_path.endswith("micromamba"):
-        cl_list = [mm_path]
+            cl_list = [mm_path]
         if cl_args is None:
             log.info("No arguments to pass to micromamba")
         else:
             cl_list.extend(cl_args)
         subprocess.run(cl_list)
+
     def create_env(self):
-        subprocess.run(['eval "$(/Users/chase/Downloads/temp/micromamba shell hook -s posix)"',
-                        '/Users/chase/Downloads/temp/micromamba',
-                        "create",
-                        "-n xtensor_env",
-                        "python=3.8"])
-        
+        env = "export MAMBA_ROOT_PREFIX='/home/chase/Downloads/temp/micromamba'"
+        subprocess.run(
+            [
+                'eval "$(/home/chase/Downloads/temp/micromamba shell hook -s posix)"',
+                "micromamba",
+                "create",
+                "-n duplipy",
+                "python=3.8",
+            ],
+            env=dict(
+                os.environ,
+                'MAMBA_ROOT_PREFIX':"/home/chase/Downloads/temp/micromamba",
+                'MAMBA_EXE':"/home/chase/Downloads/temp/micromamba",
+                'MAMBA_ROOT_PREFIX':"/home/chase/micromamba",
+                'PATH':"/home/chase/micromamba/bin:$PATH",
+            ),
+            shell=True,
+        )
 
 
-export MAMBA_ROOT_PREFIX=/Users/chase/Downloads/temp/micromamba
-eval "$(/Users/chase/Downloads/temp/micromamba shell hook -s posix)"
+a = MM()
+a.create_env()
 
 
+my_env = os.environ.copy()
+my_env['MAMBA_ROOT_PREFIX'] ="/home/chase/Downloads/temp/micromamba"
+my_env['MAMBA_EXE'] ="/home/chase/Downloads/temp/micromamba"
+my_env['MAMBA_ROOT_PREFIX'] ="/home/chase/Downloads/temp"
+my_env["PATH"] = "/home/chase/Downloads/temp:" + my_env["PATH"]
+
+subprocess.run(
+            " ".join([
+                "micromamba",
+                "create",
+                "-n duplipy",
+                "python=3.8",
+            ]),
+            env=my_env,
+            shell=True,
+        )
+
+subprocess.run(
+            " ".join([
+                "micromamba",
+                "clean --all --yes",
+            ]),
+            env=my_env,
+            shell=True,
+        )
